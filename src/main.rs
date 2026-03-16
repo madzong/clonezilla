3d2f3f7def97300b2eab9982ddf1198d5e683b8f
madzong <madzong@iusevimbtw.com>
2026-03-16 22:41:34 +0100

    initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use dialoguer::{Confirm, Select};
+use console::{Term, style};
+
 fn main() {
-    println!("Hello, world!");
+    let term = Term::stdout();
+    term.clear_screen().unwrap();
+
+    let choice = Select::new()
+        .with_prompt("Wybierz partycję")
+        .items(&["sda1", "sda2", "sda3"])
+        .default(0)
+        .interact()
+        .unwrap();
+
+    println!("{choice}");
 }
